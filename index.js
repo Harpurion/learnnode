c636b616ef9efb60d08ed17ba6e5fdf2b89c3cc5
Artur Erik Poder <artur.poder1@gmail.com>
2025-09-25 15:32:56 +0300

    Bool värk

--- index.js
+++ index.js
@@ -1,63 +1,36 @@
-function Hello() {
-    console.log("Hello, World!");
-}
+let bool = true;
+console.log(bool);
+bool = false;
+console.log(bool);  
+bool = true && false; // AND
+bool = false && true; // AND
+bool = false && false; // AND
+bool = true && true; // AND
+console.log(bool);
+bool = true || false; // OR
+bool = false || true; // OR
+bool = false || false; // OR
+bool = true || true; // OR
+console.log(bool);
+bool = !true; // NOT
+console.log(bool);
+bool = !false; // NOT
+console.log(bool);
 
-Hello();
-Hello();
+bool = (true && false || true) && (!false || (false && true));
+console.log(bool);
 
-function helloName(name) {
-    console.log(`Hello, ${name}!`);
-}
-
-helloName('Artur');
-helloName('Trent');
-helloName('Erki');
-
-function hi(name='Nameless', age='Infinite') {
-    console.log(`Hi, ${name}. You are ${age} years old.`);
-}
-hi('Artur', 17);
-hi('Trent', 17);
-hi('Erki', 17);
-hi();
-
-let sum = function (a, b) {
-    return a+b;
-}
-
-let answer = sum(4,5 );
-console.log(answer);
-console.log(sum(10, 15));
-sum (5,6);
-
-let sub = (a, b) => {
-    return a-b;
-}
-
-console.log(sub(10, 5));
-
-let mult = (a, b) => a*b;
-
-console.log(mult(10, 5));
-
-let root = a => a*a;
-
-console.log(root(10));
-
-let cool = () => console.log('cool');
-
-cool();
-
-let awesome = {
-    party: function () {
-        console.log('party!');
-    },
-    cool: () => console.log('cool'),
-    nice () {
-        console.log('nice!');
-    }
-};
-
-awesome.party();
-awesome.cool();
-awesome.nice();
+bool = 10 > 5;
+bool = 10 < 5;
+bool = 10 > 10;
+bool = 10 < 10;
+bool = 10 == 10;
+bool = '10' == 10; // Non strict will covert and check
+bool = '10' === 10; // Strict will check type and value
+bool = 10 >= 10;
+bool = 10 <= 10;
+bool = '10' != 10;
+bool = '10' !== 10;
+bool = [] == '';
+bool = [1,2, 3] == 1;
+console.log(bool);
