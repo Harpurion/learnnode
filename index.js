ba01f18dd61f56de067bb6ee4f7e3fdd1ba326ce
Artur Erik Poder <artur.poder1@gmail.com>
2025-09-25 13:50:50 +0300

    Array stuff

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-let text = 'Hello ';
-text = text + 'Artur!';
-console.log(text);
-text = 'asdasd';
-text = 'adasads';
-text = "Don't use double quotes unless needed";
-text = 'Don\'t use double quotes\n unless needed';
-console.log(text);
-let name = 'Artur';
-let age = 17;
-let sexuality = 'heterosexual';
-text = 'Your name is ' + name + ' and you are ' + age + ' years old and your sexuality is ' + sexuality + '.';
-console.log(text);
-text = `Your name is ${name} 
-and you are ${age} years old 
-and your sexuality is ${sexuality}.`;
+let array = [1, 2, 3, 'asdas', true, [1, 2, 3]];
+console.log(array);
+console.log(array[2]);
+console.log(array[5][1]);
+array[3] = 'Hello';
+console.log(array);
+console.log(array.length);
+array.push('New value');
+console.log(array);
+array.push(1, 2, 3, 'asdas', 'asdasd');
+console.log(array);
+let text = array.join('siss');
 console.log(text);
