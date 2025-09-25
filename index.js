8c1adbbd2e3d98140ea604e97fc6a2e476ded7c0
Artur Erik Poder <artur.poder1@gmail.com>
2025-09-25 14:21:26 +0300

    add obj examples

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-let array = [1, 2, 3, 'asdas', true, [1, 2, 3]];
-console.log(array);
-console.log(array[2]);
-console.log(array[5][1]);
-array[3] = 'Hello';
-console.log(array);
-console.log(array.length);
-array.push('New value');
-console.log(array);
-array.push(1, 2, 3, 'asdas', 'asdasd');
-console.log(array);
-let text = array.join('siss');
-console.log(text);
+let obj = {
+    name: 'Artur',
+    age: 17,
+    sexuality: 'hetero',
+    fruits: ['apple', 'banana', 'grape'],
+    cat: {
+        age: 7,
+        name: 'Noodle',
+        gender: 'Female'
+    },
+    parentName: 'Urmas',
+    a2sd: 'asd',
+
+};
+
+console.log(obj);
+console.log(obj.name);
+console.log(obj.fruits[1]);
+console.log(obj.cat.name);
+console.log(obj.parentName);
+console.log(Object.keys(obj));
