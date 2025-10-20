8febe4134dd5736194c460f7eabfb1c0057e9418
Artur Erik Poder <artur.poder1@gmail.com>
2025-10-20 15:16:47 +0300

    Add vue router

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 
+import { createMemoryHistory, createWebHashHistory ,createRouter } from 'vue-router';
+
+import ToDo from './pages/ToDo.vue';
+import Modals from './pages/Modals.vue';
+
+const routes = [
+    { path: '/', component: ToDo },
+    { path: '/modals', component: Modals },
+]
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
 let app = createApp(App);
+app.use(router);
 app.mount('#app');
