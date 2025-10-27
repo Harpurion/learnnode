d486d70b2cafc72723f4e2440d50e8ad81812cdb
Artur Erik Poder <artur.poder1@gmail.com>
2025-10-27 15:59:40 +0200

    Add cookie stuff without image

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,13 @@ import RouterTabs from './components/RouterTabs.vue';
 </script>
 <template>
     <RouterTabs></RouterTabs>
-    <div class="container mt-3">
+    <div v-if="$route.meta.Container === true" class="content container mt-3">
+        <Suspense>
+            <RouterView></RouterView>
+        </Suspense>
+    </div>
+    
+    <div v-else class="container mt-3">
         <div class="content">
             <Suspense>
                 <RouterView></RouterView>
---
+++ src/pages/CookieClicker.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+let cookies = ref(0);
+
+</script>
+<template>
+    <div class="Columns">
+        <div class="column has-background-primary has-text-centered">
+            <h1>{{ cookies }} cookies!</h1>
+            <div @click="cookies++">
+              <img src=""></img>
+            </div>
+        </div>
+        <div class="column has-background-warning">
+            test
+        </div>
+        <div class="column has-background-info">
+            test
+        </div>
+    </div>
+</template>
